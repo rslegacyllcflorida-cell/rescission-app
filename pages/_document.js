5c139148e25fa24b87955dccc4829d55b1060b44
rslegacyllcflorida-cell <rslegacyllcflorida@gmail.com>
2026-03-20 22:47:31 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-3942868087695151"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
